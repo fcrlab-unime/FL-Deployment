28e1eda45bcd951e7c5d374d4de3c15cf67ee0a0
MarcoGarofalo94 <garofalomarco1994@libero.it>
2025-07-29 19:06:42 +0200

    add: multi-node setup

---
+++ flare/test/workspace/multi-node/client/start_all.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+./server1/startup/start.sh
+./site-1/startup/start.sh
+./site-2/startup/start.sh
+./site-3/startup/start.sh
+./site-4/startup/start.sh
+./site-5/startup/start.sh
